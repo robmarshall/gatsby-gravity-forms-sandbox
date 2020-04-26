cbcbc5d037986912a358e65ddc702040c0d6d590
robmarshall <robertmarshllproductions@gmail.com>
2020-04-26 13:31:38 +0100

    Updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,19 @@ require("dotenv").config({
 
 console.log(`Using environment config: '${activeEnv}'`)
 
+// Set up basic auth string
+const basicAuthString =
+  "Basic " +
+  Buffer.from(
+    process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASS
+  ).toString("base64")
+
 // If we are in dev, ignore the fact that we are using a fake SSL certificate
 if (activeEnv == "development") {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 }
+
+const path = require(`path`)
 
 module.exports = {
   siteMetadata: {
@@ -29,18 +38,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     {
       resolve: "gatsby-source-gravityforms",
       options: {
@@ -58,13 +67,28 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: "gatsby-source-randomcat",
-      options: {
-        api: "a5d3ce41-1f54-4145-b630-6ce2c2afc736",
-        catCategories: "15",
-      },
-    },
+    // Setup WPGraphQL.com to be the source
+    // {
+    //   resolve: `gatsby-source-graphql`,
+    //   options: {
+    //     // This type will contain remote schema Query type
+    //     typeName: `WPGraphQL`,
+    //     // This is field under which it's accessible
+    //     fieldName: `wpgraphql`,
+    //     // Url to query from
+    //     url: `${process.env.PROTOCOL}://${process.env.BASE_URL}/graphql`,
+    //     headers: {
+    //       Authorization: basicAuthString,
+    //     },
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-source-randomcat",
+    //   options: {
+    //     api: "a5d3ce41-1f54-4145-b630-6ce2c2afc736",
+    //     limit: "1000",
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
